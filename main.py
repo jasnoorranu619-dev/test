f022125a8cce2b218457734236d682245f78f49d
jasnoorranu619-dev <jasnoorranu619@gmail.com>
2026-04-23 12:43:20 -0700

    didi pseudo codeing
    v

---
+++ main.py
@@ -0,0 +1,17 @@
+
+# Stores exactly 3 robot names and their assigned delivery zones ("Downtown", "Suburbs", "Industrial") in a dictionary.
+#Gets the total delivery distance (integer 5-500 km).
+#Gets the cargo weight for each robot (between 1 and 50 kg).
+#Gets the weather condition ("Clear", "Rain", "Storm").
+#If distance is over 300 km, any robot carries more than 50 kg, or the weather is "Storm", print "🚨 Deployment Unsafe!".
+#Otherwise, print a summary of robot names, zones, and cargo weights with the message: "🤖 Robots Ready for Delivery!".
+#
+def main():
+    #mainloop
+    print( welcome to the robot delevery choose the robot,location and the weather ) 
+
+    while true: 
+        print("\n Please choose the options blew.")
+        print()
+
+
