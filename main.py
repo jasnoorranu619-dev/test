33331e755b199d960117e46d0e5964411a207348
jasnoorranu619-dev <jasnoorranu619@gmail.com>
2026-04-28 11:23:17 -0700

    added the zones and robot names
    going to work on distance next

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 robots = {}
 zones = ["Downtown", "Suburbs", "Industrial"]
 
+# Collect robot names and zones
+for i in range(3):
+    name = input("Enter robot name: ")
+    
+    while True:
+        zone = input(f"Choose a delivery zone for {name} (Downtown, Suburbs, Industrial): ")
+        if zone in zones:
+            break
+        else:
+            print("Invalid zone. Please choose from the given options.")
+    
+    robots[name] = zone 
+
